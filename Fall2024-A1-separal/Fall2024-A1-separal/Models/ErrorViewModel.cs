c7c4c7555333a0b2da4efd7a5e03812a52e7853c
separal <separal@crimson.ua.edu>
2024-09-23 15:32:18 -0500

    Project Files

---
+++ Fall2024-A1-separal/Fall2024-A1-separal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_A1_separal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
--- Fall2024-Assignment1-separal/Fall2024-Assignment1-separal/Program.cs
+++ Fall2024-Assignment1-separal/Fall2024-Assignment1-separal/Program.cs
@@ -1,4 +1,4 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -17,7 +17,6 @@ app.UseHttpsRedirection();
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.UseStaticFiles();
 
 app.UseRouting();
 
@@ -26,4 +25,3 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
-
